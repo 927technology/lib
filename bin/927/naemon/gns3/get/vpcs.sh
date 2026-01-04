1105bf4acd9853180c98fe802a6089c877305634
Chris <cmurray@927.technology>
2026-01-04 10:49:43 -0600

    january bulk add

--- bin/927/naemon/gns3/get/vpcs.sh
+++ bin/927/naemon/gns3/get/vpcs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# because IFS sucks
+IFS=$'\n'
+
 # global variables
 _lib_version=0.4.0
 _lib_root=/usr/local/lib/bash/${_lib_version}
@@ -16,6 +19,7 @@ _os_family=el
 
 # argument variables
 _host=
+_project=
 
 # control variables
 _error_count=0
@@ -32,15 +36,39 @@ _json="{}"
         shift
         _host="${1}"
       ;;
+      -P  | --project )
+        shift
+        _project="${1}"
+      ;;
     esac
     shift
   done
 
 # main
-echo $_host
+# echo $_host
+
+if  [[ ! -z ${_host} ]]     && \
+    [[ ! -z ${_project} ]]; then
+  _json=$( gns3.get.project.vpcs --host ${_host} --project $( gns3.get.projects --host ${_host} --output id --filter ${_project} ) )
+
+
+  _exit_string+="Count: $( ${cmd_echo} ${_json} | ${cmd_jq} '. | length' )"
+
+  for vpc in $( ${cmd_echo} "${_json}" | ${cmd_jq} -c '.[]' ); do
+    # echo "$vpc"
+    # echo
+    _exit_string+="\n"
+    _exit_string+=$( ${cmd_echo} ${vpc} | ${cmd_jq} -r '"\(.node_id) - \(.name)"' )
+  done
+
+fi
+
+
+
+# _exit_string="${_json}"
 
 # exit
 [[ ${_error_count} != 0 ]] && _exit_code=${exit_crit} || _exit_code=${exit_ok}
 
-${cmd_echo} ${_exit_string}
+${cmd_echo} -e ${_exit_string}
 exit ${_exit_code}
