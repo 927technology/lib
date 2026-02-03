bc1726a4dbbd53f508342e79c95f5fefe3bfc7c8
Chris <cmurray@927.technology>
2026-02-03 16:31:53 -0600

    naemon.get.ifs #10

--- bash/0.4.0/naemon/get/ifs.f
+++ bash/0.4.0/naemon/get/ifs.f
@@ -33,7 +33,7 @@ function naemon.get.ifs() {
   done
 
   # main
-  # /bin/snmpwalk -Onq -v2c -c ${_snmp_community} ${_host} .1.3.6.1.2.1.2.2 > ${_snmp_path}/${_name}/snmp}
+  # /bin/snmpwalk -Onq -v2c -c ${_snmp_community} ${_host} .1.3.6.1.2.1.2.2 > ${_snmp_path}/${_name}/snmp
 
   # echo ${_snmp_path}/${_name}/snmp 
   # ${cmd_grep} ^.1.3.6.1.2.1.2.2.1.1.[0-9] ${_snmp_path}/${_name}/snmp
@@ -44,28 +44,28 @@ function naemon.get.ifs() {
       _json="{}"
 
      _json=$( json.set --json "${_json}" --key .index --value ${value} )
-     _json=$( json.set --json "${_json}" --key .description  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.2.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.type  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.3.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .status.admin  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.7.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .status.operatinal  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.8.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.in.error  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.4.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.speed  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.5.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.lastchange  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.9.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .description  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.2.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.type  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.3.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .status.admin  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.7.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .status.operatinal  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.8.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.in.error  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.4.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.speed  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.5.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.lastchange  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.9.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
 
-     _json=$( json.set --json "${_json}" --key .if.in.octet  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.10.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.in.unicast  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.11.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.in.nonunicast  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.12.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.in.discard  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.13.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.in.error_all  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.14.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.in.octet  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.10.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.in.unicast  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.11.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.in.nonunicast  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.12.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.in.discard  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.13.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.in.error_all  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.14.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
 
-     _json=$( json.set --json "${_json}" --key .if.out.octet  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.16.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.out.octet  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.16.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
 
-     _json=$( json.set --json "${_json}" --key .if.alias  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.18.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.out.discard  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.19.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.out.error_all  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.20.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.out.qlen  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.21.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.specific  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.22.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
-     _json=$( json.set --json "${_json}" --key .if.highspeed  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.23.${value} ${_snmp_path}/${_name}/snmp} | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.alias  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.18.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.out.discard  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.19.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.out.error_all  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.20.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.out.qlen  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.21.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.specific  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.22.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
+     _json=$( json.set --json "${_json}" --key .if.highspeed  --value $( ${cmd_grep} .1.3.6.1.2.1.2.2.1.23.${value} ${_snmp_path}/${_name}/snmp | ${cmd_awk} -F" " '{print $2}' ) )
 
 
 
